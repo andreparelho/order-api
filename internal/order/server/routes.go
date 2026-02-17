30331ea665b7d26c31e1842a24cada15f5500289
Andre Parelho <parelhoneves.andre@gmail.com>
2026-02-17 14:42:09 -0300

    feat: adicioando request id e apontando para handler

--- internal/order/server/routes.go
+++ internal/order/server/routes.go
@@ -1,12 +1,15 @@
 package server
 
 import (
+	"log"
 	"time"
 
+	order_handler "github.com/andreparelho/order-api/internal/order/handler"
 	order_service "github.com/andreparelho/order-api/internal/order/service"
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/recover"
 	"github.com/gofiber/fiber/v3/middleware/requestid"
+	"github.com/google/uuid"
 )
 
 func CreateRoute(orderService order_service.OrderService) *fiber.App {
@@ -16,10 +19,19 @@ func CreateRoute(orderService order_service.OrderService) *fiber.App {
 	})
 
 	app.Use(recover.New())
-	app.Use(requestid.New())
+	app.Use(requestid.New(requestid.Config{
+		Header: "X-Request-ID",
+		Generator: func() string {
+			reqId, err := uuid.NewRandom()
+			if err != nil {
+				log.Fatal(err)
+			}
+			return reqId.String()
+		},
+	}))
 
 	router := app.Group("/orders-api")
-	router.Post("/orders", orderService.CreateOrder())
+	router.Post("/orders", order_handler.OrderHandler(orderService))
 
 	return app
 }
