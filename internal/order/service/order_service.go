df9f85ef671819dea37f2159213a1dc13fe4512f
Andre Parelho <parelhoneves.andre@gmail.com>
2026-02-17 14:42:50 -0300

    feat: refatoracao da service e criando eventos corretamente para envio

--- internal/order/service/order_service.go
+++ internal/order/service/order_service.go
@@ -1,22 +1,21 @@
 package order_service
 
 import (
+	"context"
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"net/http"
 	"time"
 
 	order_behavior "github.com/andreparelho/order-api/internal/order/behavior"
 	order_repository "github.com/andreparelho/order-api/internal/order/repository"
 	"github.com/andreparelho/order-api/pkg/config"
 	"github.com/andreparelho/order-api/pkg/sqs"
-	"github.com/gofiber/fiber/v3"
 	"github.com/google/uuid"
 )
 
 type OrderService interface {
-	CreateOrder() fiber.Handler
+	CreateOrderService(ctx context.Context, orderRequest CreateOrderRequest, xRequestId string) error
 }
 
 type order struct {
@@ -33,54 +32,91 @@ func NewOrderService(orderRepository order_repository.OrderRepository, sqs sqs.S
 	}
 }
 
+var (
+	ErrGenerateUUID     = fmt.Errorf("erro criar um uuid")
+	ErrDatabaseInsert   = fmt.Errorf("erro ao inserir o dado na base")
+	ErrSendMessageQueue = fmt.Errorf("erro ao enviar mensagem para fila")
+	ErrMarshalEvent     = fmt.Errorf("erro ao realizar o marshal do evento sqs")
+)
+
 type CreateOrderRequest struct {
 	CustomerID  uuid.UUID `json:"customerID"`
 	TotalAmount float64   `json:"totalAmount"`
 	Currency    string    `json:"currency"`
 }
 
-func (o *order) CreateOrder() fiber.Handler {
-	return func(ctx fiber.Ctx) error {
-		var orderRequest CreateOrderRequest
-		if err := json.Unmarshal(ctx.Body(), &orderRequest); err != nil {
-			fmt.Printf("erro ao realizar o unmarshal do request, erro: %v", err)
-			return ctx.SendStatus(http.StatusBadRequest)
-		}
+type EventOrderCreatedMessage struct {
+	EventId     string         `json:"eventID"`
+	EventType   string         `json:"eventType"`
+	OccuredTime time.Time      `json:"occuredTime"`
+	Data        OrderEventData `json:"data"`
+}
 
-		orderID, err := uuid.NewRandom()
-		if err != nil {
-			fmt.Printf("erro criar um uuid, erro: %v", err)
-			return ctx.SendStatus(http.StatusInternalServerError)
-		}
+type OrderEventData struct {
+	OrderID     uuid.UUID `json:"orderID"`
+	CustomerID  uuid.UUID `json:"customerID"`
+	TotalAmount float64   `json:"totalAmount"`
+	Currency    string    `json:"currency"`
+}
 
-		order := order_repository.Order{
-			OrderID:     orderID,
-			CustomerID:  orderRequest.CustomerID,
-			Status:      order_behavior.OrderStatusCreated,
-			TotalAmount: orderRequest.TotalAmount,
-			Currency:    orderRequest.Currency,
-			CreatedAt: sql.NullTime{
-				Time:  time.Now(),
-				Valid: true,
-			},
-			UpdatedAt: sql.NullTime{
-				Time:  time.Time{},
-				Valid: false,
-			},
-		}
-
-		err = o.repository.InsertOrder(ctx.Context(), order)
-		if err != nil {
-			fmt.Printf("erro ao inserir o dado na base, erro: %v", err)
-			return ctx.SendStatus(http.StatusInternalServerError)
-		}
-
-		err = o.sqs.SendMessage(ctx.Context(), o.cfg.SQS.QueueName, "teste")
-		if err != nil {
-			fmt.Printf("erro ao enviar mensagem para fila, erro: %v", err)
-			return ctx.SendStatus(http.StatusInternalServerError)
-		}
-
-		return ctx.SendStatus(http.StatusCreated)
+func (o *order) CreateOrderService(ctx context.Context, orderRequest CreateOrderRequest, xRequestId string) error {
+	orderID, err := uuid.NewRandom()
+	if err != nil {
+		fmt.Printf("erro criar um uuid, erro: %v", err)
+		return ErrGenerateUUID
 	}
+
+	order := order_repository.Order{
+		OrderID:     orderID,
+		CustomerID:  orderRequest.CustomerID,
+		Status:      order_behavior.OrderStatusCreated,
+		TotalAmount: orderRequest.TotalAmount,
+		Currency:    orderRequest.Currency,
+		CreatedAt: sql.NullTime{
+			Time:  time.Now(),
+			Valid: true,
+		},
+		UpdatedAt: sql.NullTime{
+			Time:  time.Time{},
+			Valid: false,
+		},
+	}
+
+	err = o.repository.InsertOrder(ctx, order, xRequestId)
+	if err != nil {
+		fmt.Printf("erro ao inserir o dado na base, erro: %v", err)
+		return ErrDatabaseInsert
+	}
+
+	eventID, err := uuid.NewRandom()
+	if err != nil {
+		fmt.Printf("erro criar um uuid, erro: %v", err)
+		return ErrGenerateUUID
+	}
+
+	orderEvent := EventOrderCreatedMessage{
+		EventId:     fmt.Sprintf("event:created:{%s}", eventID.String()),
+		EventType:   "orderCreated",
+		OccuredTime: time.Now(),
+		Data: OrderEventData{
+			OrderID:     orderID,
+			CustomerID:  order.CustomerID,
+			TotalAmount: order.TotalAmount,
+			Currency:    order.Currency,
+		},
+	}
+
+	orderEventMarsh, err := json.Marshal(&orderEvent)
+	if err != nil {
+		fmt.Printf("erro ao realizar o marshal do event, erro: %v", err)
+		return ErrMarshalEvent
+	}
+
+	err = o.sqs.SendMessage(ctx, o.cfg.SQS.QueueName, string(orderEventMarsh))
+	if err != nil {
+		fmt.Printf("erro ao enviar mensagem para fila, erro: %v", err)
+		return ErrSendMessageQueue
+	}
+
+	return nil
 }
