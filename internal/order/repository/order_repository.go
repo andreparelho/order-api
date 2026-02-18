34a5008b7818cf42e3a942d28c6627c83f510a7a
Andre Parelho <parelhoneves.andre@gmail.com>
2026-02-17 22:29:43 -0300

    feat: atualializando repositories

--- internal/order/repository/order_repository.go
+++ internal/order/repository/order_repository.go
@@ -14,7 +14,7 @@ import (
 
 type OrderRepository interface {
 	InsertOrder(ctx context.Context, order Order, xRequestId string) (bool, error)
-	UpdateOrder(ctx context.Context, order Order) error
+	UpdateOrder(ctx context.Context, order Order, redisKey string) error
 }
 
 type order struct {
@@ -30,34 +30,32 @@ func NewOrderRepository(db *sql.DB, redis redis.RedisClient) OrderRepository {
 }
 
 type Order struct {
-	OrderID     uuid.UUID                  `db:"id" json:"orderID"`
-	CustomerID  uuid.UUID                  `db:"customer_id" json:"customerID"`
-	Status      order_behavior.OrderStatus `db:"status" json:"status"`
-	TotalAmount float64                    `db:"total_amount" json:"totalAmount"`
-	Currency    string                     `db:"currency" json:"currency"`
-	CreatedAt   sql.NullTime               `db:"created_at" json:"createdAt"`
-	UpdatedAt   sql.NullTime               `db:"updated_at" json:"UpdatedAt"`
+	OrderID     uuid.UUID    `db:"id" json:"orderID"`
+	CustomerID  uuid.UUID    `db:"customer_id" json:"customerID"`
+	Status      string       `db:"status" json:"status"`
+	TotalAmount float64      `db:"total_amount" json:"totalAmount"`
+	Currency    string       `db:"currency" json:"currency"`
+	CreatedAt   sql.NullTime `db:"created_at" json:"createdAt"`
+	UpdatedAt   sql.NullTime `db:"updated_at" json:"UpdatedAt"`
 }
 
 type OrderRedisInfo struct {
 	Status order_behavior.OrderStatus `json:"status"`
 }
 
-func (o *order) InsertOrder(ctx context.Context, order Order, xRequestId string) (bool, error) {
-	redisKey := fmt.Sprintf("order:req:id_%s", xRequestId)
-
+func (o *order) InsertOrder(ctx context.Context, order Order, redisKey string) (bool, error) {
 	err := o.redis.Get(ctx, redisKey)
 	if err != nil {
-		fmt.Printf("WARN: nao foi possivel buscar o dado %s do cache. Criando dado na base de dados.", redisKey)
+		fmt.Printf("\n[WARN]: nao foi possivel buscar o dado %s do cache. Criando dado na base de dados.", redisKey)
 	} else {
-		fmt.Printf("INFO: dado encotrado no cache.")
+		fmt.Printf("\n[INFO]: dado encotrado no cache.")
 		return true, nil
 	}
 
 	_, err = o.database.ExecContext(ctx, "INSERT INTO orders (id, customer_id, status, total_amount, currency, created_at, updated_at) VALUES(?, ?, ?, ?, ?, ?, ?)",
 		order.OrderID, order.CustomerID, order.Status, order.TotalAmount, order.Currency, order.CreatedAt, order.UpdatedAt)
 	if err != nil {
-		fmt.Printf("ERROR: erro ao inserir os dados no banco, erro: %v", err)
+		fmt.Printf("\n[ERROR]: erro ao inserir os dados no banco, erro: %v", err)
 		return false, err
 	}
 
@@ -67,19 +65,37 @@ func (o *order) InsertOrder(ctx context.Context, order Order, xRequestId string)
 
 	orderInfoMarsh, err := json.Marshal(&orderInfo)
 	if err != nil {
-		fmt.Printf("ERROR: erro ao realizar marshal, erro: %v", err)
+		fmt.Printf("\n[ERROR]: erro ao realizar marshal, erro: %v", err)
 		return false, err
 	}
 
 	err = o.redis.Set(ctx, redisKey, orderInfoMarsh, 10*time.Minute)
 	if err != nil {
-		fmt.Printf("ERROR: erro ao inserir no cache, erro: %v", err)
+		fmt.Printf("\n[ERROR]: erro ao inserir no cache, erro: %v", err)
 		return false, err
 	}
 
 	return false, nil
 }
 
-func (o *order) UpdateOrder(ctx context.Context, event Order) error {
+func (o *order) UpdateOrder(ctx context.Context, order Order, redisKey string) error {
+	result, err := o.database.ExecContext(ctx, "UPDATE orders SET status = ? WHERE id = ?", order.Status, order.OrderID)
+	if err != nil {
+		fmt.Printf("\n[ERROR]: erro ao inserir os dados no banco. Erro: %v", err)
+		return err
+	}
+
+	_, err = result.RowsAffected()
+	if err != nil {
+		fmt.Printf("\n[ERROR]: erro ao atualizar o dado no banco de dados. Erro: %v", err)
+		return err
+	}
+
+	err = o.redis.Del(ctx, redisKey)
+	if err != nil {
+		fmt.Printf("\n[ERROR]: erro ao deletar o dado do cache. Erro: %v", err)
+		return err
+	}
+
 	return nil
 }
